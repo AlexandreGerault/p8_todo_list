7c190e3f7ff7697298dd1e0b6d049c8de239e930
Alexandre Gérault <gerault-alexandre@orange.fr>
2022-02-01 23:35:27 +0100

    feat(ui): button link component

---
+++ apps/website-rewrite/src/Component/ButtonColor.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Component;
+
+enum ButtonColor: string
+{
+    case BLUE = 'blue';
+    case RED = 'red';
+    case GREEN = 'green';
+}
---
+++ apps/website-rewrite/src/Component/ButtonLinkComponent.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Component;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('button-link')]
+class ButtonLinkComponent
+{
+    public string $href;
+
+    public string $text;
+
+    public string $colorClasses = ' text-white bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 ';
+
+    public function mount(string $buttonColor = 'blue'): void
+    {
+        $this->colorClasses = match (ButtonColor::from($buttonColor)) {
+            ButtonColor::RED => ' text-white bg-red-600 hover:bg-red-700 focus:ring-red-500 ',
+            ButtonColor::BLUE => ' text-white bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 ',
+            ButtonColor::GREEN => ' text-white bg-green-600 hover:bg-green-700 focus:ring-green-500 ',
+        };
+    }
+}
