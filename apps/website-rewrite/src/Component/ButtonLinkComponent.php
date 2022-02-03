442b10edb8e44dc7090ae4bb2c1b7da3c47ecb32
Alexandre Gérault <gerault-alexandre@orange.fr>
2022-02-04 00:46:06 +0100

    refactor(ui): use a generic alert component

--- apps/website-rewrite/src/Component/ButtonLinkComponent.php
+++ apps/website-rewrite/src/Component/ButtonLinkComponent.php
@@ -15,12 +15,13 @@ class ButtonLinkComponent
 
     public string $colorClasses = ' text-white bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 ';
 
-    public function mount(string $buttonColor = 'blue'): void
+    public function mount(string $color = 'blue'): void
     {
-        $this->colorClasses = match (ButtonColor::from($buttonColor)) {
-            ButtonColor::RED => ' text-white bg-red-600 hover:bg-red-700 focus:ring-red-500 ',
-            ButtonColor::BLUE => ' text-white bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 ',
-            ButtonColor::GREEN => ' text-white bg-green-600 hover:bg-green-700 focus:ring-green-500 ',
+        $this->colorClasses = match (Colors::from($color)) {
+            Colors::RED => ' text-white bg-red-600 hover:bg-red-700 focus:ring-red-500 ',
+            Colors::BLUE => ' text-white bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 ',
+            Colors::GREEN => ' text-white bg-green-600 hover:bg-green-700 focus:ring-green-500 ',
+            Colors::ORANGE => ' text-white bg-orange-600 hover:bg-orange-700 focus:ring-orange-500 ',
         };
     }
 }
--- apps/website-rewrite/src/Component/Colors.php
+++ apps/website-rewrite/src/Component/Colors.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace App\Component;
 
-enum ButtonColor: string
+enum Colors: string
 {
     case BLUE = 'blue';
     case RED = 'red';
     case GREEN = 'green';
+    case ORANGE = 'orange';
 }
