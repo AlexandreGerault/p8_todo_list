52d980d7a450b112c837024d45a0d79c66bd6850
Alexandre Gérault <gerault-alexandre@orange.fr>
2022-02-25 20:35:06 +0100

    chore(components): remove unused options

--- apps/website-rewrite/src/Component/ButtonLinkComponent.php
+++ apps/website-rewrite/src/Component/ButtonLinkComponent.php
@@ -21,7 +21,6 @@ class ButtonLinkComponent
             Colors::RED => ' text-white bg-red-600 hover:bg-red-700 focus:ring-red-500 ',
             Colors::BLUE => ' text-white bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 ',
             Colors::GREEN => ' text-white bg-green-600 hover:bg-green-700 focus:ring-green-500 ',
-            Colors::ORANGE => ' text-white bg-orange-600 hover:bg-orange-700 focus:ring-orange-500 ',
         };
     }
 }
