5070692b2eaf099fb08be33bae6a6802f90a591d
Alexandre Gérault <gerault-alexandre@orange.fr>
2022-03-02 19:06:20 +0100

    tests(user): change test to reflect forbidden access

--- apps/website-rewrite/tests/Integration/Controllers/UserControllerTest.php
+++ apps/website-rewrite/tests/Integration/Controllers/UserControllerTest.php
@@ -4,83 +4,102 @@ declare(strict_types=1);
 
 namespace App\Tests\Integration\Controllers;
 
-use App\Repository\UserRepository;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class UserControllerTest extends ControllerTestCase
 {
-    public function testGuestCanSeeUserList(): void
+    public function testGuestCannotSeeUserList(): void
+    {
+        $loginUrl = $this->generator->generate('login', referenceType: UrlGeneratorInterface::ABSOLUTE_URL);
+        $userListUrl = $this->generator->generate('user_list');
+
+        $this->client->request('GET', $userListUrl);
+
+        $this->assertResponseRedirects($loginUrl);
+    }
+
+    public function testAUserCannotSeeUserList(): void
     {
         $userListUrl = $this->generator->generate('user_list');
+
+        $this->actingAsUser();
+        $this->client->request('GET', $userListUrl);
+
+        $this->assertResponseStatusCodeSame(403);
+    }
+
+    public function testAGuestCannotCreateAUser(): void
+    {
+        $this->actingAsUser();
+
+        $userCreateUrl = $this->generator->generate('user_create');
+        $this->client->request('GET', $userCreateUrl);
+
+        $this->assertResponseStatusCodeSame(403);
+    }
+
+    public function testAUserCannotCreateAUser(): void
+    {
+        $loginUrl = $this->generator->generate('login', referenceType: UrlGeneratorInterface::ABSOLUTE_URL);
+        $userCreateUrl = $this->generator->generate('user_create');
+
+        $this->client->request('GET', $userCreateUrl);
+
+        $this->assertResponseRedirects($loginUrl);
+    }
+
+    public function testAGuestCannotEditUser(): void
+    {
+        $loginUrl = $this->generator->generate('login', referenceType: UrlGeneratorInterface::ABSOLUTE_URL);
+        $userEditUrl = $this->generator->generate('user_edit', ['id' => 1]);
+
+        $this->client->request('GET', $userEditUrl);
+
+        $this->assertResponseRedirects($loginUrl);
+//        $this->client->submitForm('Modifier', [
+//            'user' => [
+//                'username' => 'Utilisateur modifié',
+//                'password' => ['first' => 'password', 'second' => 'password'],
+//                'email' => 'nouvel-utilisateur@email.fr'
+//            ],
+//        ]);
+//        $crawler = $this->client->followRedirect();
+//        $this->assertRouteSame('user_list');
+//        $this->assertStringContainsString("L'utilisateur a bien été modifié.", $crawler->html());
+    }
+
+    public function testAUserCannotEditAnotherUser(): void
+    {
+        $this->actingAsUser();
+
+        $userEditUrl = $this->generator->generate('user_edit', ['id' => 1]);
+        $this->client->request('GET', $userEditUrl);
+
+        $this->assertResponseStatusCodeSame(403);
+//
+//        $this->client->submitForm('Modifier', [
+//            'user' => [
+//                'username' => 'Utilisateur modifié',
+//                'password' => ['first' => 'password', 'second' => 'password'],
+//                'email' => 'nouvel-utilisateur@email.fr'
+//            ],
+//        ]);
+//        $crawler = $this->client->followRedirect();
+//        $this->assertRouteSame('user_list');
+//        $this->assertStringContainsString("L'utilisateur a bien été modifié.", $crawler->html());
+
+    }
+
+    public function testAnAdminCanSeeUserList(): void
+    {
+        $userListUrl = $this->generator->generate('user_list');
+
+        $this->actingAsAdmin();
+
         $crawler = $this->client->request('GET', $userListUrl);
 
         $this->assertResponseIsSuccessful();
-        $this->assertStringContainsString("user@localhost", $crawler->text());
-    }
-
-    public function testAUserCanSeeUserList(): void
-    {
-        $this->actingAsUser($this->client);
-
-        $userListUrl = $this->generator->generate('user_list');
-        $crawler = $this->client->request('GET', $userListUrl);
-
-        $this->assertResponseIsSuccessful();
-        $this->assertStringContainsString("user@localhost", $crawler->text());
-    }
-
-    public function testAGuestCanCreateAUser(): void
-    {
-        $this->actingAsUser($this->client);
-
-        $this->assertCanCreateUser();
-    }
-
-    public function testAUserCanCreateAUser(): void
-    {
-        $this->assertCanCreateUser();
-    }
-
-    public function testAGuestCanEditUser(): void
-    {
-        $userEditUrl = $this->generator->generate('user_edit', ['id' => 1]);
-        $crawler = $this->client->request('GET', $userEditUrl);
-
-        $this->assertResponseIsSuccessful();
-        $this->assertStringContainsString('Modifier', $crawler->html());
-
-        $this->client->submitForm('Modifier', [
-            'user' => [
-                'username' => 'Utilisateur modifié',
-                'password' => ['first' => 'password', 'second' => 'password'],
-                'email' => 'nouvel-utilisateur@email.fr'
-            ],
-        ]);
-        $crawler = $this->client->followRedirect();
-        $this->assertRouteSame('user_list');
-        $this->assertStringContainsString("L'utilisateur a bien été modifié.", $crawler->html());
-    }
-
-    public function testAUserCanEditAnotherUser(): void
-    {
-        $this->actingAsUser($this->client);
-
-        $userEditUrl = $this->generator->generate('user_edit', ['id' => 1]);
-        $crawler = $this->client->request('GET', $userEditUrl);
-
-        $this->assertResponseIsSuccessful();
-        $this->assertStringContainsString('Modifier', $crawler->html());
-
-        $this->client->submitForm('Modifier', [
-            'user' => [
-                'username' => 'Utilisateur modifié',
-                'password' => ['first' => 'password', 'second' => 'password'],
-                'email' => 'nouvel-utilisateur@email.fr'
-            ],
-        ]);
-        $crawler = $this->client->followRedirect();
-        $this->assertRouteSame('user_list');
-        $this->assertStringContainsString("L'utilisateur a bien été modifié.", $crawler->html());
-
+        $this->assertStringContainsString('user@localhost', $crawler->html());
     }
 
     private function assertCanCreateUser(): void
