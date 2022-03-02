42b35af9796fe5e3df6fd7c6ee5301e7d5bbd3d7
Alexandre Gérault <gerault-alexandre@orange.fr>
2022-03-02 23:19:33 +0100

    feat(task): assign a task to the acting user

--- apps/website-rewrite/src/DataFixtures/TaskFixture.php
+++ apps/website-rewrite/src/DataFixtures/TaskFixture.php
@@ -22,7 +22,7 @@ class TaskFixture extends Fixture implements DependentFixtureInterface
     public function load(ObjectManager $manager): void
     {
         $manager->persist($this->makeTask('Titre tâche 1', 'Contenu tâche 1', '2022-02-16 00:00:00'));
-        $manager->persist($this->makeTask('Titre tâche 2', 'Contenu tâche 2', '2022-02-17 00:00:00'));
+        $manager->persist($this->makeTask('Titre tâche 2', 'Contenu tâche 2', '2022-02-17 00:00:00', 2));
         $manager->persist($this->makeTask('Titre tâche 32', 'Contenu tâche 3', '2022-02-17 00:00:00', 3));
 
         $manager->flush();
